eaefbcee3d4f4887df3c25450a2bb82a0f8f01e1
Tainá Luria <taina.luria@outlook.com>
2020-10-07 00:55:40 +0000

    Initial commit

---
+++ Filme.java
@@ -0,0 +1,24 @@
+package cinemonitor;
+
+public class filme {
+	private String titulo;
+	private int ano, tipo;
+	public String getTitulo() {
+		return titulo;
+ 	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+}
